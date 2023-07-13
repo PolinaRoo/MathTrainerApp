5d2df3a7876b6cea488397273aa9c5b9434f6520
PolinaRoo <polina.roo10@gmail.com>
2023-07-13 23:04:46 +0300

    Change button "Back", add shadows to the second screen

--- MathTrainerApp/TrainViewController.swift
+++ MathTrainerApp/TrainViewController.swift
@@ -9,10 +9,31 @@ import Foundation
 import UIKit
 
 final class TrainViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet var buttonsAnswer: [UIButton]!
+    
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
             print(type)
+        }
+    }
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureButtons()
+    }
+    
+    //MARK: Methods
+    private func configureButtons() {
+        // add shadow
+        buttonsAnswer.forEach { button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
         }
     }
 }
