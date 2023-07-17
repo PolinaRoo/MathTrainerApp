292d8a32d1556931656c2ddee6c0a0d3263c7e79
PolinaRoo <polina.roo10@gmail.com>
2023-07-18 00:17:12 +0300

    Added logic to questions, added count on second screen, added counts on first screen for each operation

--- MathTrainerApp/TrainViewController.swift
+++ MathTrainerApp/TrainViewController.swift
@@ -8,11 +8,18 @@
 import Foundation
 import UIKit
 
+//для отправки на предыдущий экран
+protocol DataDelegate {
+    func sendCount(count: String, type: MathTypes)
+}
+
 final class TrainViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    
     
     // MARK: - Properties
     var type: MathTypes = .add {
@@ -30,12 +37,17 @@ final class TrainViewController: UIViewController {
         }
     }
     
-    private var firstNumber = 0
-    private var secondNumber = 0
+    private var firstNumber: Int = 0
+    private var secondNumber: Int = 0
+    private var firstNumberDivide: Double = 0
+    private var secondNumberDivide: Double = 0
     private var sign: String = ""
+    var delegate: DataDelegate? = nil
+    
     private var count: Int = 0 {
         didSet {
             print("Count: \(count)")
+            countLabel.text = "Count: \(count)"
         }
     }
     
@@ -47,9 +59,14 @@ final class TrainViewController: UIViewController {
             return firstNumber - secondNumber
         case .multiply:
             return firstNumber * secondNumber
-        case .divide:
-            return firstNumber / secondNumber
+        default:
+            return 0
         }
+    }
+    
+    private var answerDivide: Double {
+        print("\(firstNumberDivide)    \(secondNumberDivide)   \(firstNumberDivide / secondNumberDivide)       \((firstNumberDivide / secondNumberDivide * 10).rounded() / 10)")
+        return (firstNumberDivide/secondNumberDivide * 10).rounded() / 10
     }
     
     // MARK: - Life cycle
@@ -69,6 +86,15 @@ final class TrainViewController: UIViewController {
         check(answer: sender.titleLabel?.text ?? "", for: sender)
     }
     
+    @IBAction func sendData(_ sender: Any) {
+        if (delegate != nil) {
+            let countString: String = String(count)
+            delegate!.sendCount(count: countString, type: type)
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    
     //MARK: Methods
     private func configureButtons() {
         let buttonsAnswer = [leftButton,rightButton]
@@ -86,31 +112,55 @@ final class TrainViewController: UIViewController {
         }
         
         let isRightButton = Bool.random()
-        var randomAnswer: Int
-        repeat {
-            randomAnswer = Int.random(in: (answer - 10)...(answer+10))
-        } while randomAnswer == answer
         
+        if type != .divide {
+            var randomAnswer: Int
+            repeat {
+                randomAnswer = Int.random(in: (answer - Int.random(in: 1...9))...(answer + Int.random(in: 1...9)))
+            } while randomAnswer == answer
+            rightButton.setTitle(isRightButton ? String(Int(randomAnswer)) : String(answer) , for: .normal)
+            leftButton.setTitle(isRightButton ? String(answer) : String(Int(randomAnswer)), for: .normal)
+        } else {
+            var randomAnswerDivide: Double
+            repeat {
+                randomAnswerDivide = Double.random(in: (answerDivide - Double.random(in: 1.0...9.9))...(answerDivide + Double.random(in: 1.0...9.9)))
+            } while randomAnswerDivide == answerDivide
+            rightButton.setTitle(isRightButton ? String((randomAnswerDivide * 10).rounded() / 10) : String(answerDivide) , for: .normal)
+            leftButton.setTitle(isRightButton ? String(answerDivide) : String((randomAnswerDivide * 10).rounded()/10), for: .normal)
+        }
         
-        rightButton.setTitle(isRightButton ? String(randomAnswer) : String(answer) , for: .normal)
-        leftButton.setTitle(isRightButton ? String(answer) : String(randomAnswer), for: .normal)
-       
     }
     
     private func configureQuestion() {
-        firstNumber = Int.random(in: 1...99)
-        secondNumber = Int.random(in: 1...99)
-        let question: String = "\(firstNumber) \(sign) \(secondNumber) = "
-        questionLabel.text = question
+        var question: String = ""
+        if type != .divide {
+            firstNumber = Int.random(in: 1...99)
+            secondNumber = Int.random(in: 1...99)
+            if type == .multiply {
+                secondNumber = Int.random(in: 2...9)
+            }
+            question = "\(firstNumber) \(sign) \(secondNumber) = "
+            questionLabel.text = question
+        } else {
+            firstNumberDivide = Double.random(in: 1...99)
+            secondNumberDivide = Double.random(in: 2...9)
+            firstNumberDivide.round(.towardZero)
+            secondNumberDivide.round(.towardZero)
+            question = "\(Int(firstNumberDivide)) \(sign) \(Int(secondNumberDivide)) = "
+            questionLabel.text = question
+        }
     }
     
     private func check(answer: String, for button: UIButton) {
-        let isRightAnswer = Int(answer) == self.answer
-        
+        let isRightAnswer: Bool
+        if type != .divide {
+            isRightAnswer = Int(answer) == self.answer
+        } else {
+            isRightAnswer = Double(answer) == self.answerDivide
+        }
         button.backgroundColor = isRightAnswer ? .green : .red
-        
         if isRightAnswer {
-            var isSecondAnswer = rightButton.backgroundColor == .red ||
+            let isSecondAnswer = rightButton.backgroundColor == .red ||
             leftButton.backgroundColor == .red
             
             count += isSecondAnswer ? 0 : 1
